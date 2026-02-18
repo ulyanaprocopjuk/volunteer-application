c656f2a64873c09f5ac19baa5945492b85c196cf
Ulyana Procopjuk <ulianaprocop@mail.ru>
2026-02-18 21:03:52 +0300

    Add IOS client project

---
+++ client/volunteerClient/volunteerClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  volunteerClient
+//
+//  Created by Ульяна Прокопюк on 18.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ client/volunteerClient/volunteerClient/volunteerClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  volunteerClientApp.swift
+//  volunteerClient
+//
+//  Created by Ульяна Прокопюк on 18.02.26.
+//
+
+import SwiftUI
+
+@main
+struct volunteerClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
